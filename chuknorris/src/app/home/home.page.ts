260054aedc162550c188c77d1a33e780a460254f
totalnemtom <matybal.m@gmail.com>
2021-04-23 20:57:40 +0200

    capacitor build android

---
+++ chuknorris/src/app/home/home.page.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  constructor() {}
+
+  displayJoke() {
+    this.deleteJokes();
+    const url = `https://api.icndb.com/jokes/random`;
+
+    const jokeDiv = document.getElementsByClassName('jokediv')[0];
+    const jokeP = document.createElement('p');
+    jokeP.setAttribute('class', 'joke');
+    jokeDiv.appendChild(jokeP);
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((joke) => (jokeP.textContent = joke.value.joke))
+      .catch((err) => console.error(err));
+  }
+
+  deleteJokes() {
+    const jokeDiv = document.getElementsByClassName('jokediv')[0];
+    jokeDiv.innerHTML = '';
+  }
+}
